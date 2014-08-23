f18d14cf6ba2f8d1476e7ec9d7720a78e1f7f720
crotten <wildrunts@gmail.com>
2014-08-23 18:36:08 -0400

    things

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -2,11 +2,11 @@ var postTmpl = [
   "<% _.each(posts, function(element, index, list) { %>",
   "<article data-postid=\"<% element._id %>\" class=\"post\">",
     "<h3><%= element.nameinput %></h3>",
-    "<p><%= element.experienceinput %></p>",
+    "<p><%= element.experienceinput %> years of experience!</p>",
     "<p><%= element.techinput %></p>",
     "<p><%= element.textinput %></p>",
     "<p><%= element.emailinput %></p>",
-    "<p><button class=\"editPost\">Update</button> <button class=\"nevermind\">Scratch That</button></p>",
+    "<p><button class=\"editPost\">Update</button> <button class=\"nevermind\">Scratch That</button> <button class=\"answerQuestion\">Answer!</button></p>",
 
 
 
